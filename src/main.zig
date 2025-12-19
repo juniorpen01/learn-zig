ad0de1056b399e1a1b7a2d3633a2e090dda169c9
juniorpen01 <01juniorpen@gmail.com>
2025-12-20 07:33:25 +0800

    Learn about declaring variables and mutability, undefined, destructing, tupless, arrays, and "vector"s (?)

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,11 @@
 const part_1 = @import("part_1.zig");
 const part_2 = @import("part_2.zig");
 const part_3 = @import("part_3.zig");
+const part_4 = @import("part_4.zig");
 
 pub fn main() !void {
     try part_1.part_1();
     try part_2.part_2();
     try part_3.part_3();
+    try part_4.part_4();
 }
