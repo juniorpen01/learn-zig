9f60c11f0b3e78b2a90a93e6731e422aa417fe1c
juniorpen01 <01juniorpen@gmail.com>
2025-12-19 18:51:55 +0800

    Learn various kinds of comments, printing, structs, etc.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # learn-zig
+
 An account of my preliminary learnings of Zig.
+
+## References
+
+1. <https://ziglang.org/documentation/0.15.2>
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,29 @@
+//! main.zig
+
+/// std
+// std
+/// std's
 const std = @import("std");
 
+/// main
+/// i hate u
+/// 🫠
 pub fn main() !void {
     try std.fs.File.stdout().writeAll("what wrong with this\n");
-    std.debug.print("hello world\n", .{});
+    std.debug.print("hello world\n", .{}); // hello world
+    // hello world again
+
+    const foo = Vec2.zero();
+    std.debug.print("hello {}", .{foo});
 }
+
+/// what is this
+const Vec2 = struct {
+    //! what is this
+    x: i32,
+    y: i32,
+
+    pub fn zero() Vec2 {
+        return Vec2{ .x = 0, .y = 0 };
+    }
+};
