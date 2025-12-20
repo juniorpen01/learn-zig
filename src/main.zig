1b3f2b6bb7efbe1ecda0471c7dd6b83bb2019af7
juniorpen01 <01juniorpen@gmail.com>
2025-12-20 11:59:36 +0800

    Set up part 5 and write some unit test syntax
    Taking a break for now to analyze my options

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,13 @@ const part_1 = @import("part_1.zig");
 const part_2 = @import("part_2.zig");
 const part_3 = @import("part_3.zig");
 const part_4 = @import("part_4.zig");
+const part_5 = @import("part_5.zig");
+const std = @import("std");
 
 pub fn main() !void {
     try part_1.part_1();
     try part_2.part_2();
     try part_3.part_3();
     try part_4.part_4();
+    try part_5.part_5();
 }
---
+++ src/part_5.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+
+pub fn part_5() !void {}
+
+test "wtf" {
+    try std.testing.expect(false);
+}
