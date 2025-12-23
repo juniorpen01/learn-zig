bec79de899f272088b34c9c3d3efccea892ecf75
juniorpen01 <01juniorpen@gmail.com>
2025-12-23 21:06:58 +0800

    Refactor and correctly deallocate memory in build script

--- build.zig
+++ build.zig
@@ -11,11 +11,20 @@ pub fn build(b: *std.Build) !void {
 fn setupExe(b: *std.Build, name: []const u8, target: std.Build.ResolvedTarget) !void {
     const allocator = std.heap.page_allocator;
 
-    const exe = b.addExecutable(.{ .name = name, .root_module = b.createModule(.{ .root_source_file = b.path(try std.fmt.allocPrint(allocator, "src/bin/{s}.zig", .{name})), .target = target }) });
+    const path = try std.fmt.allocPrint(allocator, "src/bin/{s}.zig", .{name});
+
+    const run_name = try std.fmt.allocPrint(allocator, "run {s}", .{name});
+    const run_desc = try std.fmt.allocPrint(allocator, "Run {s}", .{name});
+
+    const exe = b.addExecutable(.{ .name = name, .root_module = b.createModule(.{ .root_source_file = b.path(path), .target = target }) });
     b.installArtifact(exe);
 
-    const run_step = b.step(try std.fmt.allocPrint(allocator, "run {s}", .{name}), try std.fmt.allocPrint(allocator, "Run {s}", .{name}));
+    const run_step = b.step(run_name, run_desc);
     const run_cmd = b.addRunArtifact(exe);
     run_step.dependOn(&run_cmd.step);
-    run_step.dependOn(b.getInstallStep());
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    allocator.free(path);
+    allocator.free(run_name);
+    allocator.free(run_desc);
 }
--- src/bin/main.zig
+++ src/bin/main.zig
@@ -3,4 +3,5 @@ const std = @import("std");
 pub fn main() !void {
     std.debug.print("idiot\n", .{});
     std.debug.print("idiot\n", .{});
+    std.debug.print("idiot\n", .{});
 }
