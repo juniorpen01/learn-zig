a3e745525f76b9dcf37a87c7030c32626b58bba6
juniorpen01 <01juniorpen@gmail.com>
2025-12-23 12:12:28 +0800

    Learn to set up build system

--- build.zig
+++ build.zig
@@ -1,5 +1,12 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b; // stub
+    const target = b.standardTargetOptions(.{});
+    const exe = b.addExecutable(.{ .name = "learn_zig", .root_module = b.createModule(.{ .root_source_file = b.path("src/main.zig"), .target = target }) });
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run learn_zig");
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+    run_step.dependOn(b.getInstallStep());
 }
