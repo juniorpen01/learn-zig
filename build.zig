5da41e47e3c312c24544015fcaf71274e541d387
juniorpen01 <01juniorpen@gmail.com>
2025-12-23 12:54:09 +0800

    Fix "multiple targets" and add prelminary binary

--- build.zig
+++ build.zig
@@ -1,14 +1,16 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
-    try setupExe(b, "main");
+    const target = b.standardTargetOptions(.{});
+
+    try setupExe(b, "main", target);
+    try setupExe(b, "preliminary", target);
 }
 
-/// HACK: Just copy paste the whole thing into another func; holy crap no dealloac
-fn setupExe(b: *std.Build, name: []const u8) !void {
+/// HACK: Just copy paste the whole thing into another func; holy crap no dealloc
+fn setupExe(b: *std.Build, name: []const u8, target: std.Build.ResolvedTarget) !void {
     const allocator = std.heap.page_allocator;
 
-    const target = b.standardTargetOptions(.{});
     const exe = b.addExecutable(.{ .name = name, .root_module = b.createModule(.{ .root_source_file = b.path(try std.fmt.allocPrint(allocator, "src/bin/{s}.zig", .{name})), .target = target }) });
     b.installArtifact(exe);
 
